3121dbf05d1de8dec318cdf26fd788e4a2b0a702
shineLeeee <a40357321311@163.com>
2017-02-26 11:12:01 +0800

    2.26

--- src/test.scala
+++ src/test.scala
@@ -1,8 +1,41 @@
 object test {
   def main(args: Array[String]){
     println("hello scala") 
-    println("sdf")
-    print("hello world")
-    print("it")
+    val num:Any="it"
+    var x=2
+    println(if (x==1) "true" else "false")
+    val m=Array("welcome","come","to","my space")
+    
+    for (i <- m)
+      print(i+" ")
+      
+    println()
+    println(1.to(10).map(_*10))
+    
+    val a1=Array(1,2,3)
+    println(a1.reverse(1))
+     
+    val v = for (i <- 1 to 10) yield i * 10
+    println(v)
+    
+    def multi(x:Int,y:Int):Int={
+      x*y*13
+      
+    }
+    def m4(f:(Int,Int)=>Int)=f(2,6)
+    val f2=(x:Int,y:Int)=>2+6
+    println("m4:"+m4(f2))
+      
+  
+    
+    println(multi(3,4))
+    val func1 = (x:Int,y:Double) => (y,x)
+    def m1(x:Int,y:Int)=x+x*y
+    println("结果是："+m1(5,7))
+    val m2=(x:Int,y:Int)=>{x+y}
+    println("结果为："+m2(3,7))
+    
+
+      
   } 
 }    
